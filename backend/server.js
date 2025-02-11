f7698aa184f7f2153f1d9a988776d2f259f8a203
darkblue0555 <36949891+epicfacedood@users.noreply.github.com>
2025-02-11 11:05:06 +0800

    fix backend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ connectDB();
 connectCloudinary();
 
 //CORS configuration
-const cors = require("cors");
+
 const corsOptions = {
   origin: "*",
   credentials: true, //access-control-allow-credentials:true
